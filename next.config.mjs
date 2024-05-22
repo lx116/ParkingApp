01577fb30f0059a7d69b660de862e744716c4611
luisvelez <luisenriquevelezsantos@gmail.com>
2024-05-21 22:56:23 -0500

    aaaa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath:'/ParkingApp',
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
